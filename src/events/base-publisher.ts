d0d830a9c67d1906241244ba35e1b1365e78c379
Edddo123 <edo.miraqian@gmail.com>
2021-10-31 02:19:17 +0400

    Updates

--- src/events/base-publisher.ts
+++ src/events/base-publisher.ts
@@ -8,7 +8,7 @@ interface Event {
 
 export abstract class Publisher<T extends Event> {
   abstract subject: T["subject"];
-  protected client: Stan; 
+  protected client: Stan;
 
   constructor(client: Stan) {
     this.client = client;
@@ -20,6 +20,7 @@ export abstract class Publisher<T extends Event> {
         if (err) {
           reject(err);
         }
+        console.log("Message published to " + this.subject);
         resolve();
       });
     });
--- src/events/subjects.ts
+++ src/events/subjects.ts
@@ -1,3 +1,4 @@
 export enum Subjects {
   BuyerCreated = "buyer:created",
+  BuyerBanned = "buyer:banned"
 }
